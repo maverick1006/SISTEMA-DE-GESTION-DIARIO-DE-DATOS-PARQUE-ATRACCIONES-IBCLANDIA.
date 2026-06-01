e35a065bcf9c626e7d4ade75796bed3217b6b063
Lillq919 <martin.lira.rodriguez@gmail.com>
2026-06-01 05:45:12 -0400

    agregadas validaciones requisitos fila

--- funciones.c
+++ funciones.c
@@ -795,6 +795,10 @@ void agregarVisitanteAFila(struct Atraccion *atraccion, struct Visitante *visita
     int pos;
     if(atraccion == NULL || visitante == NULL) return;
 
+    if(visitante->altura < atraccion->alturaMinima || visitante->edad < atraccion->edadMinima) {
+        printf("\nVisitante no puede hacer file, ya que no cumple con requisitos de altura y/o edad.\n");
+        return;
+
     if(atraccion->fila->cantidadEnFila >= atraccion->fila->maxCola){
         printf("\nFila llena.\n");
         return;
